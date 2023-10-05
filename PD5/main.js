ce612f796a1e51dc8a81cb77966aab44e5ae0ca5
radeit <80596399+radeit@users.noreply.github.com>
2023-10-05 11:16:50 +0300

    PD5 commit

---
+++ PD5/main.js
@@ -0,0 +1,24 @@
+let number = 12;
+let otherNumber = 45.89;
+let textOne = 'Beautiful JavaScript World';
+let textTwo = "Beautiful JavaScript World";
+let textThree = `Beautiful JavaScript World`;
+let isTrue = (true == false);
+let isEqual = (2 >= 0);
+let petName = undefined;
+let petAge = null;
+let human = {
+humanName: undefined,
+humanAge: null
+};
+
+console.log(typeof number);
+console.log(typeof otherNumber);
+console.log(typeof textOne);
+console.log(typeof textTwo);
+console.log(typeof textThree);
+console.log(typeof isTrue);
+console.log(typeof isEqual);
+console.log(typeof petName);
+console.log(typeof petAge);
+console.log(typeof human);
