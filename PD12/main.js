16fb26fa545ed0587d26888ac191e8d4bf5cedc6
radeit <80596399+radeit@users.noreply.github.com>
2023-10-11 09:43:01 +0300

    PD12 commit

---
+++ PD12/main.js
@@ -0,0 +1,15 @@
+// 1 Uzduotis
+let pirmadienis = 5;
+let antradienis = 6;
+let treciadienis = 4;
+let ketvirtadienis = 5;
+let penktadienis = 4;
+let result = pirmadienis + antradienis + treciadienis + ketvirtadienis + penktadienis;
+
+console.log('Paskaitų skaičius per savaitę - ' + result,'. Tai sudaro ' +result*45, 'minučių.');
+
+// 2 Uzduotis
+let puodelis = 7;
+let dezute = 3;
+
+console.log('Pilnų dėžučių skaičius: ' + Math.floor(puodelis/dezute),'.','Nesupakuotų puodelių skaičius: '+ Math.ceil(dezute/puodelis),'.');
